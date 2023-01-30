b6923e0a4854641308806ecef24c47268b784f40
agustinahoyossg <agustina.hoyos@smallgiants.agency>
2023-01-30 19:20:00 -0300

    vue config js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV
-=== "production"
-? "entrega-final"
-: "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/entrega-final/" : "/",
 })
